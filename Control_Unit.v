b1fe71995424867a0c26bf99231d0987e88852fe
zhzdeng <1123284216@qq.com>
2016-04-15 00:11:00 +0800

    Control Unit 1.2

--- Control_Unit.v
+++ Control_Unit.v
@@ -29,7 +29,7 @@ module Control_Unit(
     output reg [2:0] ALUOp, // ALU的运算类型
     output reg ALUSrcB, // ALU的数据端选择
     output reg ALUM2Reg, // 只有lw：1
-    output reg PCSrc, // halt : 0 其他1
+    output reg PCSrc, // beq PCSrc = zero others: 0
     output reg DataMemRW // 1:写入数据存储器（只有lw），0：读
     );
 
@@ -44,7 +44,7 @@ module Control_Unit(
 				ALUOp <= 3'b000;
 				ALUSrcB <= 0;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 0;
 				end
 			6'b000001: begin // addi
@@ -56,7 +56,7 @@ module Control_Unit(
 				ALUOp <= 3'b000;
 				ALUSrcB <= 1;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 0;
 				end
 			6'b000010: begin // sub
@@ -68,7 +68,7 @@ module Control_Unit(
 				ALUOp <= 3'b001;
 				ALUSrcB <= 0;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 0;
 				end
 			6'b010000: begin // ori
@@ -80,7 +80,7 @@ module Control_Unit(
 				ALUOp <= 3'b011;
 				ALUSrcB <= 1;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 0;
 				end
 			6'b010001: begin // and
@@ -92,7 +92,7 @@ module Control_Unit(
 				ALUOp <= 3'b100;
 				ALUSrcB <= 0;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 0;		
 				end
 			6'b010010: begin // or
@@ -104,7 +104,7 @@ module Control_Unit(
 				ALUOp <= 3'b011;
 				ALUSrcB <= 0;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 0;		
 				end
 			6'b100000: begin // move
@@ -116,7 +116,7 @@ module Control_Unit(
 				ALUOp <= 3'b000;
 				ALUSrcB <= 0;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 0;
 				end
 			6'b100110: begin // sw
@@ -128,7 +128,7 @@ module Control_Unit(
 				ALUOp <= 3'b000;
 				ALUSrcB <= 1;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 0;
 				end
 			6'b100111: begin // lw
@@ -140,7 +140,7 @@ module Control_Unit(
 				ALUOp <= 3'b000;
 				ALUSrcB <= 1;
 				ALUM2Reg <= 1;
-				PCSrc <= 1;
+				PCSrc <= 0;
 				DataMemRW <= 1;
 				end
 			6'b110000: begin // beq
@@ -152,7 +152,7 @@ module Control_Unit(
 				ALUOp <= 3'b001;
 				ALUSrcB <= 0;
 				ALUM2Reg <= 0;
-				PCSrc <= 1;
+				PCSrc <= zero;
 				DataMemRW <= 0;
 				end
 			6'b111111: begin // halt
